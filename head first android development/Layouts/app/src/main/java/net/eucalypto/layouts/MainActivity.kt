a7b58dbe7f3f2ea09f8eb9ab5cc55c6133de3b11
Viktor Papara <developer@eucalypto.net>
2021-04-04 21:05:28 +0200

    Add FrameLayout example
    #13

--- head first android development/Layouts/app/src/main/java/net/eucalypto/layouts/MainActivity.kt
+++ head first android development/Layouts/app/src/main/java/net/eucalypto/layouts/MainActivity.kt
@@ -1,11 +1,11 @@
 package net.eucalypto.layouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_linear_layout)
+        setContentView(R.layout.activity_main_frame_layout)
     }
 }
