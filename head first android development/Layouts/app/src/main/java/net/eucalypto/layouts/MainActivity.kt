f2ec9a639ef0bc20f3ada78e232647913a0a8d4e
Viktor Papara <developer@eucalypto.net>
2021-04-12 18:14:50 +0200

    Add ToggleButton, Switch, CheckBox examples
    And a constraint layout example
    
    #13

--- head first android development/Layouts/app/src/main/java/net/eucalypto/layouts/MainActivity.kt
+++ head first android development/Layouts/app/src/main/java/net/eucalypto/layouts/MainActivity.kt
@@ -2,10 +2,9 @@ package net.eucalypto.layouts
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +27,37 @@ class MainActivity : AppCompatActivity() {
         val input = (view as EditText).text.toString()
 
         Toast.makeText(this, "You just typed: \"$input\"", Toast.LENGTH_LONG).show()
+    }
 
+    fun onToggleButtonClicked(view: View) {
+        val toggled = (view as ToggleButton).isChecked
+
+        if (toggled) {
+            toast("You just turned the ToggleButton on")
+        } else {
+            toast("You just turned the ToggleButton off")
+        }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun onSwitchCompatClicked(view: View) {
+        val isOn = (view as SwitchCompat).isChecked
+
+        if (isOn)
+            toast("You just switched ON the Switch")
+        else
+            toast("You just switched OFF the Switch")
+    }
+
+    fun onCheckBoxClicked(view: View) {
+        val isChecked = (view as CheckBox).isChecked
+
+        if (isChecked)
+            toast("You want some ${getString(R.string.checkbox)}")
+        else
+            toast("You DON'T want some ${getString(R.string.checkbox)}")
     }
 }
