dea607a8578bd56e065b0612249900f81bb4fb64
Viktor Papara <developer@eucalypto.net>
2021-06-04 13:29:16 +0200

    feat: Make recyclerview display photo titles
    Finalize RecyclerView by implementing the PhotoAdapter and corresponding PhotoHolder. Also, use ViewModel to persist the API call across configuration changes. Previously, the Fragment would toggle such a network API request each time it was (re-)created even after config changes. That was not a desirable behavior.
    
    #19

--- big_nerd_ranch_guide_android/PhotoGallery/app/src/main/java/net/eucalypto/bignerdranch/photogallery/PhotoGalleryFragment.kt
+++ big_nerd_ranch_guide_android/PhotoGallery/app/src/main/java/net/eucalypto/bignerdranch/photogallery/PhotoGalleryFragment.kt
@@ -4,21 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import net.eucalypto.bignerdranch.photogallery.databinding.FragmentPhotoGalleryBinding
-import timber.log.Timber
 
 class PhotoGalleryFragment : Fragment() {
+
+    private val viewModel: PhotoGalleryViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val flickrLiveData = FlickrFetcher().fetchPhotos()
-        flickrLiveData.observe(this) { galleryItems ->
-            Timber.d("Response Received: $galleryItems")
-        }
+
     }
 
     override fun onCreateView(
@@ -38,6 +39,38 @@ class PhotoGalleryFragment : Fragment() {
 
         with(viewBinding) {
             photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+            viewModel.galleryItemLiveData.observe(viewLifecycleOwner) { galleryItems ->
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            }
+        }
+
+
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+        RecyclerView.Adapter<PhotoAdapter.PhotoHolder>() {
+
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PhotoHolder {
+            val textView = TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val title = galleryItems[position].title
+            holder.bindTitle(title)
+        }
+
+        override fun getItemCount(): Int {
+            return galleryItems.size
+        }
+
+        private class PhotoHolder(itemTextView: TextView) :
+            RecyclerView.ViewHolder(itemTextView) {
+            val bindTitle: (CharSequence) -> Unit = itemTextView::setText
         }
     }
 
