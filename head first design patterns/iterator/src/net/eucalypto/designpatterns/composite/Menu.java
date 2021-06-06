a0a848b970a3ef84c99df307a23ea462f9374bc8
Viktor Papara <developer@eucalypto.net>
2021-06-06 15:20:03 +0200

    Use Composite pattern for alternative implementation
    In the Composite pattern we can treat the Menus and MenuItems uniformly and put them in a tree structure that allows to print in a recursive and elegant way.
    
    #16

---
+++ head first design patterns/iterator/src/net/eucalypto/designpatterns/composite/Menu.java
@@ -0,0 +1,52 @@
+package net.eucalypto.designpatterns.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu implements MenuComponent {
+
+  List<MenuComponent> menuComponents = new ArrayList<>();
+  String name;
+  String description;
+
+  public Menu(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  @Override
+  public void add(MenuComponent menuComponent) {
+    menuComponents.add(menuComponent);
+  }
+
+  @Override
+  public void remove(MenuComponent menuComponent) {
+    menuComponent.remove(menuComponent);
+  }
+
+  @Override
+  public MenuComponent getChild(int i) {
+    return menuComponents.get(i);
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public void print() {
+    System.out.print("\n" + getName());
+    System.out.println(", " + getDescription());
+    System.out.println("---------------------");
+
+    for (var menuComponent : menuComponents) {
+      menuComponent.print();
+    }
+  }
+}
