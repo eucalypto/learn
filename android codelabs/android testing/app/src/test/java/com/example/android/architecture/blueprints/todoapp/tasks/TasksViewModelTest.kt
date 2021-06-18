991b2e3a6e2ed4f1ec694c5f12313b219b8dfea7
Viktor Papara <developer@eucalypto.net>
2021-06-18 14:54:43 +0200

    Refactor test: add @Before setup method
    #25

--- android codelabs/android testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android codelabs/android testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -6,6 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,14 +14,19 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 internal class TasksViewModelTest {
 
+    // Executes each task synchronously using Architecture Components.
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var viewModel: TasksViewModel
+
+    @Before
+    fun setUpViewModel() {
+        viewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun `addNewTask sets new task event`() {
-        val viewModel =
-            TasksViewModel(ApplicationProvider.getApplicationContext())
-
         viewModel.addNewTask()
 
         val value = viewModel.newTaskEvent.getOrAwaitValue()
@@ -30,10 +36,6 @@ internal class TasksViewModelTest {
 
     @Test
     fun `setFilter ALL_TASKS tasks Add view is visible`() {
-
-        val viewModel =
-            TasksViewModel(ApplicationProvider.getApplicationContext())
-
         viewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
         val isVisible = viewModel.tasksAddViewVisible.getOrAwaitValue()
