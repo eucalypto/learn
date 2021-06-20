96801cb7112354e9d51fa7f030f6f682aa0d31fd
Viktor Papara <developer@eucalypto.net>
2021-06-20 20:32:50 +0200

    Use Espresso library to test views
    Now that we did the heavy lifting of decoupling TaskDetailFragment from its dependencies that we can inject doubles and test only the fragment, we use Espresso to check the views.
    
    #25

--- android codelabs/android testing/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ android codelabs/android testing/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -2,6 +2,9 @@ package com.example.android.architecture.blueprints.todoapp.taskdetail
 
 import FakeAndroidTestRepository
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
@@ -10,11 +13,13 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.IsNot.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 class TaskDetailFragmentTest {
@@ -27,22 +32,66 @@ class TaskDetailFragmentTest {
         ServiceLocator.tasksRepository = repository
     }
 
-    @ExperimentalCoroutinesApi
     @After
     fun cleanUpRepository() = runBlockingTest {
         ServiceLocator.resetRepository()
     }
 
-    @ExperimentalCoroutinesApi
     @Test
     fun activeTaskDetails_displayedInUi() = runBlockingTest {
 
         val activeTask = Task(
-            "Active Task", "AndroidX rocks", false
+            "Active Task", "AndroidX Rocks", false
         )
         repository.saveTask(activeTask)
 
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+        onView(withId(R.id.task_detail_title_text))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text))
+            .check(matches(withText("Active Task")))
+        onView(withId(R.id.task_detail_description_text))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text))
+            .check(matches(withText("AndroidX Rocks")))
+        onView(withId(R.id.task_detail_complete_checkbox))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox))
+            .check(matches(not(isChecked())))
     }
+
+    @Test
+    fun completedTaskDetails_DisplayedInUi() = runBlockingTest {
+        // GIVEN - Add completed task to the DB
+        val task = Task(
+            "Destroy the Ring",
+            "Bring the Ring to Mount Doom and toss it in the fire",
+            true
+        )
+        repository.saveTask(task)
+
+        // WHEN - Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(task.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        onView(withId(R.id.task_detail_title_text))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text))
+            .check(matches(withText("Destroy the Ring")))
+
+        onView(withId(R.id.task_detail_description_text))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text))
+            .check(matches(withText("Bring the Ring to Mount Doom and toss it in the fire")))
+
+        onView(withId(R.id.task_detail_complete_checkbox))
+            .check(matches(isChecked()))
+        onView(withId(R.id.task_detail_complete_checkbox))
+            .check(matches(isDisplayed()))
+    }
+
 }
