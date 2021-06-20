8ff15c01ebaf70d5386bc2b604b9117c2033a634
Viktor Papara <developer@eucalypto.net>
2021-06-20 10:38:55 +0200

    Add empty starting Android project: Sandbox
    #30

---
+++ Sandbox/README.md
@@ -0,0 +1,5 @@
+# Sandbox App
+
+This app is for trying out stuff in Android without messing up other apps.
+
+[Git Log (Issue/DevelopmentThread)](https://github.com/eucalypto/learn/issues/30)
---
+++ Sandbox/app/src/main/java/net/eucalypto/sandbox/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.eucalypto.sandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.eucalypto.sandbox.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
