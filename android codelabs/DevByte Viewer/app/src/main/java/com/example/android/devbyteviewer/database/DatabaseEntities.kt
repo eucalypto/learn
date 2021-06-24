ebf79c6f996e39006c9ade8373118b94fc5c47ce
Viktor Papara <developer@eucalypto.net>
2021-06-24 19:42:38 +0200

    Add DatabaseVideo as a database entity
    This app teaches how to separate the models:
    - Domain model is what the app business logic uses exclusively
    - Data transfer model is what the network package uses to receive/parse the network requests
    - and now the Database model are the entities for Room
    
    It makes a lot of sense to separate them into separate models because all of them have very distinct responsibilities although they seem to look similar.
    If we put them in a single object, it would be cluttered with the network transfer and database stuff.
    
    #34

--- android codelabs/DevByte Viewer/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ android codelabs/DevByte Viewer/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,29 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity
+data class DatabaseVideo(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
+    }
+}
