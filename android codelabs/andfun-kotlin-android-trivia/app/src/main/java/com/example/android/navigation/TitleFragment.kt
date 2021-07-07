8289c22091f181953acd23a43d0b1574cd33fb5a
Viktor Papara <developer@eucalypto.net>
2021-07-07 17:36:09 +0200

    Add navigation between fragments
    Instead of "hardcoding" the title fragment in the activity view, we define there a NavHostFragment that will host the fragments. It needs a navigation graph defining the destinations and actions.
    
    The navigation component with its NavController and Navigation class offer many ways to define navigation in onclicklisteners.
    
    #37

--- android codelabs/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android codelabs/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -15,6 +17,21 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return FragmentTitleBinding.inflate(inflater, container, false).root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding: FragmentTitleBinding =
+            DataBindingUtil.getBinding<FragmentTitleBinding>(view)!!
+
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                TitleFragmentDirections.actionTitleFragmentToGameFragment().actionId
+            )
+        )
+
     }
 
 }
