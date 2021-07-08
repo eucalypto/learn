e1f4587d40d3bd2d99d64581477dafc88898d63b
Viktor Papara <developer@eucalypto.net>
2021-07-08 16:14:31 +0200

    Add a navigation drawer to the app
    A navigation drawer can be activated by swiping from outside a vertical screen side, or when in the home destination clicking the "up" button that is displayed with a hamburger icon.
    
    NavigationUI helps us connect the components together: action bar, drawer layout (parent layout for content and drawer), the navigation drawer, and the NavController.
    
    #37

--- android codelabs/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ android codelabs/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,11 +19,18 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
@@ -32,12 +39,20 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
 
-        val navController = findNavController(R.id.navHostFragment)
+        navController = findNavController(R.id.navHostFragment)
+        drawerLayout = binding.drawerLayout
 
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(
+            navController,
+            drawerLayout
+        )
+        NavigationUI.setupWithNavController(
+            binding.navigationDrawer,
+            navController
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.navHostFragment).navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
