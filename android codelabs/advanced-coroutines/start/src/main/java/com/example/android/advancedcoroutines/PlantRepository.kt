2e133ffecb1245fb9ae9faef91c8b3218c5567e2
Viktor Papara <developer@eucalypto.net>
2021-07-18 11:04:51 +0200

    Refactor: use switchMap to build plantswithGrowZone Live Data
    In switchMap you have to return a complete LiveData object (and not simply the content of one). On the one hand, this means more work for the developer, but on the other hand, we can use an extension function liveData {} to build a live data object using coroutines.
    
    #39

--- android codelabs/advanced-coroutines/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ android codelabs/advanced-coroutines/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -16,13 +16,16 @@
 
 package com.example.android.advancedcoroutines
 
+import androidx.annotation.AnyThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
 import com.example.android.advancedcoroutines.util.CacheOnSuccess
 import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * Repository module for handling data operations.
@@ -60,14 +63,15 @@ class PlantRepository private constructor(
      * Fetch a list of [Plant]s from the database that matches a given [GrowZone].
      * Returns a LiveData-wrapped List of Plants.
      */
-    fun getPlantsWithGrowZone(growZone: GrowZone) = liveData {
-        val plantsGrowZoneLiveData =
-            plantDao.getPlantsWithGrowZoneNumber(growZone.number)
-        val customSortOrder = plantListSortOrderCache.getOrAwait()
-        emitSource(plantsGrowZoneLiveData.map { plantList ->
-            plantList.applySort(customSortOrder)
-        })
-    }
+    fun getPlantsWithGrowZone(growZone: GrowZone) =
+        plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+            .switchMap { plantList ->
+                liveData {
+                    val customSortOrder = plantListSortOrderCache.getOrAwait()
+                    emit(plantList.applyMainSafeSort(customSortOrder))
+                }
+            }
+
 
     /**
      * Returns true if we should make a network request.
@@ -127,8 +131,13 @@ class PlantRepository private constructor(
                 ).also { instance = it }
             }
     }
-}
 
+    @AnyThread
+    suspend fun List<Plant>.applyMainSafeSort(customSortOrder: List<String>) =
+        withContext(defaultDispatcher) {
+            this@applyMainSafeSort.applySort(customSortOrder)
+        }
+}
 
 private fun List<Plant>.applySort(customSortOrder: List<String>): List<Plant> {
     return sortedBy { plant ->
@@ -139,3 +148,4 @@ private fun List<Plant>.applySort(customSortOrder: List<String>): List<Plant> {
         ComparablePair(positionForItem, plant.name)
     }
 }
+
