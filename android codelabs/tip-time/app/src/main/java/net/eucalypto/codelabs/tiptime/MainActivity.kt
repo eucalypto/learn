8d2a45f34328da196d977717ac65a41e61234a3a
Viktor Papara <developer@eucalypto.net>
2021-08-10 18:41:13 +0200

    Implement tip calculation and display result
    #46

--- android codelabs/tip-time/app/src/main/java/net/eucalypto/codelabs/tiptime/MainActivity.kt
+++ android codelabs/tip-time/app/src/main/java/net/eucalypto/codelabs/tiptime/MainActivity.kt
@@ -1,11 +1,43 @@
 package net.eucalypto.codelabs.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.eucalypto.codelabs.tiptime.databinding.MainActivityBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
