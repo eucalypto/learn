296d91c89cf704d27535e457a0e72c11c3f44ad7
Viktor Papara <developer@eucalypto.net>
2021-09-28 16:10:17 +0200

    Remove auto-generated code and write from scratch
    according to tutorial :)
    
    #48

--- android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
+++ android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
@@ -3,36 +3,26 @@ package net.eucalypto.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import net.eucalypto.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard("Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text("Hello $name")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
