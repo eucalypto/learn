4a14fbbeffadc25bb80f90bdac27dfee884a519a
Viktor Papara <developer@eucalypto.net>
2021-09-30 11:21:27 +0200

    Add more elements to MessageCard
    like using Row and Column, Image and using modifiers.
    
    #48

--- android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
+++ android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
@@ -3,26 +3,51 @@ package net.eucalypto.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            MessageCard(Message(author = "Android", body = "Jetpack Compose"))
         }
     }
 }
 
 @Composable
-fun MessageCard(name: String) {
-    Text("Hello $name")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        Column {
+            Text(msg.author)
+            Spacer(Modifier.height(4.dp))
+            Text(msg.body)
+        }
+    }
 }
+
+data class Message(val author: String, val body: String)
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+    MessageCard(Message(author = "Android", body = "Hey Take a look at Jetpack Compose"))
 }
