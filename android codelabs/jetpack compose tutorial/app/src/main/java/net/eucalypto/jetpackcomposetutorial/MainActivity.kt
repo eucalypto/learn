f831ee910a4293f0df8d4abbc8824c06da553eb9
Viktor Papara <developer@eucalypto.net>
2021-09-30 14:56:24 +0200

    Add more styling and use App theme
    Also, add a second preview by doubling the `@Preview` annotations, to have a night mode previews as well.
    
    #48

--- android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
+++ android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package net.eucalypto.jetpackcomposetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,12 +17,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.eucalypto.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message(author = "Android", body = "Jetpack Compose"))
+            JetpackComposeTutorialTheme() {
+                MessageCard(Message(author = "Android", body = "Jetpack Compose"))
+            }
         }
     }
 }
@@ -32,22 +39,41 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(Modifier.width(8.dp))
 
         Column {
-            Text(msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(Modifier.height(4.dp))
-            Text(msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message(author = "Android", body = "Hey Take a look at Jetpack Compose"))
+    JetpackComposeTutorialTheme {
+        MessageCard(Message(author = "Android", body = "Hey Take a look at Jetpack Compose"))
+    }
 }
