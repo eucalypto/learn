feace0d6980f419cd8e0c0047276d09f0978084d
Viktor Papara <developer@eucalypto.net>
2021-10-05 10:50:20 +0200

    Add animations when expanding text.
    We animate the color using animateColorAsState(), which gives us color animation out of the box when a new color is chosen -> no boilerplate code whatsoever!
    
    We also animate the box size using animateContentSize().
    
    #48

--- android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
+++ android codelabs/jetpack compose tutorial/app/src/main/java/net/eucalypto/jetpackcomposetutorial/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -17,6 +19,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,6 +59,9 @@ fun MessageCard(msg: Message) {
         Spacer(Modifier.width(8.dp))
 
         var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
 
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
@@ -65,7 +71,14 @@ fun MessageCard(msg: Message) {
             )
             Spacer(Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
