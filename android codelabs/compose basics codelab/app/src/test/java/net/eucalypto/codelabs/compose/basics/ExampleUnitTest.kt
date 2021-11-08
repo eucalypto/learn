623a6d6d9a167112f0934cdd3b1515b23b085df2
Viktor Papara <developer@eucalypto.net>
2021-11-08 22:06:10 +0100

    Start empty Jetpack Compose project for Basics Codelabs #49

---
+++ android codelabs/compose basics codelab/app/src/test/java/net/eucalypto/codelabs/compose/basics/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package net.eucalypto.codelabs.compose.basics
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4f, 2f + 2f)
+    }
+}
